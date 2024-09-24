d4c3e85d60fa95d83d6a4f569ac1c3a5cdc46289
Kevin Carroll <abacus822@gmail.com>
2024-09-23 22:07:07 -0400

    refactor Error enum to allow to derive Clone

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,14 @@
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
 use clap::Error as clap_error;
 use derive_more::{Display, From};
 use reqwest::Error as req_err;
 use std::error::Error as StdError;
+use std::sync::Arc;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug, From, Display)]
+#[derive(Debug, From, Display, Clone)]
 pub enum Error {
 	// -- Internals
 	#[display("--log-level must be a one of [info|error|debug] but got {}", _0)]
@@ -25,9 +28,20 @@ pub enum Error {
 
 	// -- Externals
 	#[from]
-	Clap(clap_error),
+	Clap(Arc<clap_error>),
 	#[from]
-	ReqwestError(req_err),
+	ReqwestError(Arc<req_err>),
 }
 
 impl StdError for Error {}
+
+impl IntoResponse for Error {
+	fn into_response(self) -> Response {
+		println!("->> {:<4} - {self:?}", "INTO_RES");
+		// placeholder for axum response
+		let mut resp = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+		// Insert the Error into the response
+		resp.extensions_mut().insert(self);
+		resp
+	}
+}
